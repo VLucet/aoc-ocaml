c0d5365b416e1a2f5f7124f5ce1df24314fd5d79
vlucet <valentin.lucet@gmail.com>
2024-01-04 22:34:34 -0500

    base day 7

---
+++ puzzles/day_7.ml
@@ -0,0 +1,26 @@
+(* PART 1 *)
+
+let input_test_file = "inputs/2021/day_7_test.txt"
+let input_file = "inputs/2021/day_7.txt"
+
+let read_file file =
+  In_channel.with_open_bin file In_channel.input_all
+
+let parse_content file =
+  let contents = read_file file in
+    String.split_on_char ',' contents
+    |> List.map int_of_string
+
+let solve_part1 file = 
+  parse_content file
+
+let result_test = solve_part1 input_test_file
+let result = solve_part1 input_file
+
+(* PART 2 *)
+
+let solve_part2 file =  
+  parse_content file
+
+let result_test_2 = solve_part2 input_test_file
+let result_2 = solve_part2 input_file
